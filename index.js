8b8e84484331d3adb363d36ad87794c36be48344
MattDorchies <MDorchiesPro@gmail.com>
2026-01-29 17:22:50 +0100

    Prout

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-const Ships=["Mandalay","ASP Explorer","Diamondback Explorer"];
-console.log(Ships)
+const ships = [
+    {
+        name: "Mandalay",
+        price: 17000000,
+        availability: true,
+    },
+    {
+        name: "Diamondback Explorer",
+        price: 8500000,
+        availability: false,
+    },
+    {
+        name: "ASP Explorer",
+        price: 11000000,
+        availability: true,
+    },
+    {
+        name: "Krait MKII",
+        price: 48000000,
+        availability: true,
+    },
+    {
+        name: "Caspian Explorer",
+        price: 198000000,
+        availability: false
+    }
+];
+
+const shipyard = document.getElementById("ships");
+console.log(shipyard);
+const htmlConst = ships.map(ship =>`
+    <div class="shipArray">
+        <p>${ship.name}</p>
+        ${ship.availability ? `<p>${ship.price} cr</p>` : ``}
+        <div class="pelletCenter">
+            <div class="pellet ${ship.availability ? `green` : `red`}"></div>
+        </div>
+    </div>
+    `).join("")
+    console.log(htmlConst);
+shipyard.innerHTML = htmlConst;    
+
+//<p>${ship.price} cr</p>
